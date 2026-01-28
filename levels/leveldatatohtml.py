0a7952d3776f4aa4d2579fa0000db8ff062cdb97
kostiantynisnotavailable <204969@stmichaelcollege.nl>
2026-01-28 10:31:57 +0100

    start of python program for levels

---
+++ levels/leveldatatohtml.py
@@ -0,0 +1,29 @@
+print("Make sure you loaded the right level data!")
+# Put the level data after "data=", the rest will be requested with input()
+data={'level':[
+  "00000021111111111111111111111120",
+  "00000021111111111111113311111200",
+  "20000021111111111111230031111200",
+  "12000002111111111113000003133000",
+  "12000002111123311330000003300002",
+  "13222003133200023000000300000021",
+  "30000003300300023000323000233021",
+  "20000031200000030203113000212031",
+  "20332313002230000003111200312031",
+  "30000021200313323231111133113021",
+  "30002002300311111111111111112021",
+  "13222000000312213331222131120002",
+  "11111332233112113131212131120002",
+  "11111111111112113111212133112221",
+  "11111111111111113331111111111111",
+  "11111111111111111111111111111111"],
+  'sp':[3,4],
+  'ep':[29,12],
+  'cols':[(48,8,89),(70,62,106),(118,117,163),(200,255,255)]}
+#...
+ratetype="-1"
+levelname=input("Name of level: ")
+authorname=input("Name of author: ")
+while ratetype not in ["0","1","2"]:ratetype=input("Rate type (none: 0, cloud: 1, flower: 2): ")
+imagepath=input("Path to image (e. g. level images/example.png): ")
+print("\n<!DOCTYPE html>\n<html>\n<head>\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\">\n\t<style>\n\t\t.dropdown {\n\t\t\tposition: relative;\n\t\t\tpadding: 20px 30px;\n\t\t\tborder-radius: 4px;\n\t\t\tbackground-color: #000;\n\t\t\tcolor: #FFF;\n\t\t\tdisplay: inline-block;\n\t\t}\n\t\t.dropdown-content {\n\t\t\tdisplay: none;\n\t\t\tposition: absolute;\n\t\t\tcolor: #000;\n\t\t\tbackground-color: #f9f9f9;\n\t\t\tborder-radius: 4px;\n\t\t\tmin-width: 550px;\n\t\t\tbox-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);\n\t\t\tpadding: 12px 16px;\n\t\t\tfont-size: 16px;\n\t\t}\n\t\t.dropdown:hover .dropdown-content {\n\t\t\tdisplay: block;\n\t\t}\n\t</style\n</head>")
